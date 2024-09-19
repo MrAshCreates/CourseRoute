42bc6ba03d48ef606bfbe4403dd21ec31c4b62d1
Asher Winstead <111993154+MrAshCreates@users.noreply.github.com>
2024-09-19 12:21:37 -0500

    Create db_manager.py

---
+++ courseroute/database/db_manager.py
@@ -0,0 +1,26 @@
+# eduscheduler/database/db_manager.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from configparser import ConfigParser
+
+from eduscheduler.database.models import Base
+
+def get_config(filename='config.ini', section='database'):
+    parser = ConfigParser()
+    parser.read(filename)
+    db_config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db_config[param[0]] = param[1]
+    return db_config
+
+def create_session():
+    db_config = get_config()
+    engine_str = f"postgresql://{db_config['user']}:{db_config['password']}@" \
+                 f"{db_config['host']}:{db_config['port']}/{db_config['database']}"
+    engine = create_engine(engine_str)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
