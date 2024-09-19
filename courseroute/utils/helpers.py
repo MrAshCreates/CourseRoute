e9e5bd0078e0c9789ec4b667451397f4ed173442
Asher Winstead <111993154+MrAshCreates@users.noreply.github.com>
2024-09-19 12:28:03 -0500

    Create helpers.py

---
+++ courseroute/utils/helpers.py
@@ -0,0 +1,9 @@
+# eduscheduler/utils/helpers.py
+
+def get_math_credits_needed(student):
+    # Return the number of math credits needed for the student
+    return 4  # Example value
+
+def get_required_courses_per_student(student=None):
+    # Return the number of courses each student should be enrolled in
+    return 7  # Example value
