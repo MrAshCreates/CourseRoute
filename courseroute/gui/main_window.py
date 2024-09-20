6b43803e80d0c5179e07d8516fc3af5fb18c3ea1
Asher Winstead <111993154+MrAshCreates@users.noreply.github.com>
2024-09-19 22:08:08 -0500

    Update main_window.py

--- courseroute/gui/main_window.py
+++ courseroute/gui/main_window.py
@@ -13,7 +13,17 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.session = create_session()
+        # In __init__ method of MainWindow
+
+        self.ui.addStudentButton.clicked.connect(self.open_add_student_form)
+        self.ui.editStudentButton.clicked.connect(self.open_edit_student_form)
+        self.ui.deleteStudentButton.clicked.connect(self.delete_student)
+        # Repeat for courses, teachers, rooms
         
+        # Menu actions for import/export
+        self.ui.actionImport_PowerSchool.triggered.connect(self.import_powerschool_data)
+        self.ui.actionExport_PowerSchool.triggered.connect(self.export_to_powerschool)
+
         # Connect menu actions
         self.ui.actionExit.triggered.connect(self.close)
         self.ui.actionImport_Data.triggered.connect(self.import_data)
@@ -43,22 +53,28 @@ class MainWindow(QMainWindow):
         else:
             QMessageBox.warning(self, "Scheduling Error", "Could not generate schedule.")
 
+    # In display_schedule method
+
     def display_schedule(self, schedule):
-        # Display the schedule in the GUI
-        # For example, populate a QTableWidget
         self.ui.scheduleTableWidget.clearContents()
         self.ui.scheduleTableWidget.setRowCount(0)
-
+        headers = ["Student ID", "Student Name", "Course", "Section", "Time Slot", "Teacher", "Room"]
+        self.ui.scheduleTableWidget.setColumnCount(len(headers))
+        self.ui.scheduleTableWidget.setHorizontalHeaderLabels(headers)
+    
         for student_id, courses in schedule.items():
+            student = self.session.query(Student).get(student_id)
             for course_info in courses:
                 row_position = self.ui.scheduleTableWidget.rowCount()
                 self.ui.scheduleTableWidget.insertRow(row_position)
                 self.ui.scheduleTableWidget.setItem(row_position, 0, QTableWidgetItem(str(student_id)))
-                self.ui.scheduleTableWidget.setItem(row_position, 1, QTableWidgetItem(course_info['course']))
-                self.ui.scheduleTableWidget.setItem(row_position, 2, QTableWidgetItem(str(course_info['section'])))
-                self.ui.scheduleTableWidget.setItem(row_position, 3, QTableWidgetItem(str(course_info['timeslot'])))
-                self.ui.scheduleTableWidget.setItem(row_position, 4, QTableWidgetItem(course_info['teacher']))
-                self.ui.scheduleTableWidget.setItem(row_position, 5, QTableWidgetItem(course_info['room']))
+                self.ui.scheduleTableWidget.setItem(row_position, 1, QTableWidgetItem(student.name))
+                self.ui.scheduleTableWidget.setItem(row_position, 2, QTableWidgetItem(course_info['course']))
+                self.ui.scheduleTableWidget.setItem(row_position, 3, QTableWidgetItem(str(course_info['section'])))
+                self.ui.scheduleTableWidget.setItem(row_position, 4, QTableWidgetItem(str(course_info['timeslot'])))
+                self.ui.scheduleTableWidget.setItem(row_position, 5, QTableWidgetItem(course_info['teacher']))
+                self.ui.scheduleTableWidget.setItem(row_position, 6, QTableWidgetItem(course_info['room']))
+
 
     @pyqtSlot()
     def import_data(self):
@@ -85,3 +101,19 @@ class MainWindow(QMainWindow):
     def load_students(self):
         # Load student data into the student table
         pass
+
+    def delete_student(self):
+        selected_items = self.ui.studentTableWidget.selectedItems()
+        if selected_items:
+            student_id = selected_items[0].text()
+            student = self.session.query(Student).get(student_id)
+            if student:
+                self.session.delete(student)
+                self.session.commit()
+                self.load_students()
+                
+    def import_powerschool_data(self):
+        from courseroute.integrations.powerschool_api import import_data_from_powerschool
+        import_data_from_powerschool(self.session)
+        self.load_all_data()
+    
